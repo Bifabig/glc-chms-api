c5dc5dc9f3c197d3584cf952ed0334a2770b5861
Bifabig <biftubig@gmail.com>
2023-11-02 12:17:04 +0300

    add controller test

---
+++ test/controllers/api/v1/churches_controller_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class Api::V1::ChurchesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_v1_churches_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get api_v1_churches_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get api_v1_churches_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get api_v1_churches_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get api_v1_churches_destroy_url
+    assert_response :success
+  end
+end
