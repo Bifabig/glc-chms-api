04ae4ec06eaf18a363582f195216deb3c8ea3395
Bifabig <biftubig@gmail.com>
2023-11-02 12:16:43 +0300

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,32 +13,28 @@ church_1 = user_1.churches.create!(
   location: Faker::Address.city
 )
 
-member_1 = user_1.members.create!(
+member_1 = church_1.members.create!(
   name: Faker::Name.name,
   photo: "https://robohash.org/sitsequiquia.png?size=300x300&set=set1",
   joined_at: Faker::Date.between(from: '2008-09-23', to: '2023-09-25'),
   address: Faker::Address.city,
-  phone_number: Faker::PhoneNumber.cell_phone,
-  church_id: church.id
+  phone_number: Faker::PhoneNumber.cell_phone
 )
 
-team_1 = user_1.teams.create!(
+team_1 = church_1.teams.create!(
   name: Faker::Sports::Football.team,
   main_leader_name: Faker::Name.name,
   sub_leader_name: Faker::Name.name,
-  member_id: member_1.id,
-  church_id: church.id
+  established_at: Faker::Date.between(from: '2008-09-23', to: '2023-09-25')
 )
 
-program_1 = user_1.programs.create!(
+program_1 = church_1.programs.create!(
   name: Faker::ProgrammingLanguage.name,
   date: Faker::Date.between(from: '2008-09-23', to: '2023-09-25'),
-  attendance_id: attendance_1.id,
-  church: church_1,
   team_id: team_1.id
 )
 
-attendance_1 = user_1.attendances.create!(
+attendance_1 = program_1.create_attendance(
   name: Faker::Name.name,
   att_taker: Faker::Name.name,
   is_present: true
