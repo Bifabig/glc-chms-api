1376f3e1a312456691d87290659555b1c491bc00
Bifabig <biftubig@gmail.com>
2023-11-02 12:17:13 +0300

    add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show, :create, :new, :destroy]
+      resources :churches, only: [:index, :show, :create, :new, :destroy]
+      resources :members, only: [:index, :show, :create, :new, :destroy]
+      resources :teams, only: [:index, :show, :create, :new, :destroy]
+      resources :programs, only: [:index, :show, :create, :new, :destroy]
+      resources :attendances, only: [:index, :show, :create, :new, :destroy]
+    end
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
