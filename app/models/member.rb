697c0ee81c47af4f6312eecc94a19bb6802d9480
Bifabig <biftubig@gmail.com>
2023-11-02 12:16:25 +0300

    update member and program models

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ApplicationRecord
   belongs_to :church
-  has_and_belongs_to_many :teams, foreign_key: 'team_id', dependent: :destroy
+  has_and_belongs_to_many :teams, foreign_key: 'member_id', dependent: :destroy
 
   validates :name, :photo, :address, :phone_number, :joined_at, presence: true
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,6 +1,6 @@
 class Program < ApplicationRecord
   belongs_to :church
-  has_and_belongs_to_many :teams, foreign_key: 'team_id', dependent: :destroy
+  has_and_belongs_to_many :teams, foreign_key: 'program_id', dependent: :destroy
   has_one :attendance
 
   validates :name, :date, presence: true
