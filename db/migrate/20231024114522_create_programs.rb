c67061f0e0145fb6daabf3a3a961e0fb9ce32997
Bifabig <biftubig@gmail.com>
2023-12-29 09:15:10 +0300

    modify program and attendance migrations

--- db/migrate/20231024114522_create_programs.rb
+++ db/migrate/20231024114522_create_programs.rb
@@ -2,17 +2,11 @@ class CreatePrograms < ActiveRecord::Migration[7.0]
   def change
     create_table :programs do |t|
       t.string :name
+      t.string :attendance_taker
       t.date :date
       t.references :church, null: false, foreign_key: true
 
       t.timestamps
     end
-
-    # create_table :attendances do |t|
-    #   t.string :att_taker
-    #   t.references :program, null: false, foreign_key: true
-
-    #   t.timestamps
-    # end
   end
 end
