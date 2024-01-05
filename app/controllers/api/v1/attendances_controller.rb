38fb9527c943e263c7796beb7740dea857d62538
Bifabig <biftubig@gmail.com>
2024-01-05 12:01:56 +0300

    modify attendance controller

--- app/controllers/api/v1/attendances_controller.rb
+++ app/controllers/api/v1/attendances_controller.rb
@@ -9,15 +9,17 @@ class Api::V1::AttendancesController < ApplicationController
 
   def show
     @attendance = Attendance.find(params[:id])
-    render json: @attendance
+    options = {
+      include: %i[program]
+    }
+    render json: AttendanceSerializer.new(@attendance, options)
   end
 
   def create
     existing_attendance = Attendance.find_by(attendance_params) # Find existing record based on given params
 
-    if existing_attendance
-      existing_attendance.destroy # Destroy if found
-    end
+    # Destroy if found
+    existing_attendance&.destroy
     @attendance = Attendance.new(attendance_params)
 
     if @attendance.save
@@ -34,7 +36,10 @@ class Api::V1::AttendancesController < ApplicationController
     @attendance = Attendance.find(params[:id])
 
     if @attendance.update(attendance_params)
-      render json: @attendance
+      options = {
+        include: %i[program]
+      }
+      render json: { attendance: AttendanceSerializer.new(@attendance, options), message: 'success' }, status: :ok
     else
       render json: { error: 'Error updating attendance data' }, status: :unprocessable_entity
     end
@@ -42,8 +47,15 @@ class Api::V1::AttendancesController < ApplicationController
 
   def destroy
     @attendance = Attendance.find(params[:id])
-    @attendance.destroy
-    head :no_content
+    if @attendance.destroy
+      @attendances = Attendance.all.order(created_at: :desc)
+      options = {
+        include: %i[program]
+      }
+      render json: AttendanceSerializer.new(@attendances, options)
+    else
+      render json: { error: 'Error Deleting member data' }, status: :unprocessable_entity
+    end
   end
 
   private
