22618cd8b2cf17e4b6b724696fd7dbfb9d67115a
Bifabig <biftubig@gmail.com>
2024-01-05 12:02:14 +0300

    modify attendance serializer

--- app/serializers/attendance_serializer.rb
+++ app/serializers/attendance_serializer.rb
@@ -2,5 +2,5 @@ class AttendanceSerializer
   include JSONAPI::Serializer
   belongs_to :program
 
-  attributes :id, :member_name, :status, :remark
+  attributes :id, :member_name, :status, :remark, :created_at, :updated_at
 end
