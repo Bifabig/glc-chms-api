bdb35d54f4e4cc6fcd13f76f26604775320bcecf
Bifabig <biftubig@gmail.com>
2024-01-16 17:09:01 +0300

    setup sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,22 +42,31 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  # config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   # config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   user_name:      ENV['SENDMAIL_USERNAME'],
+  #   password:       ENV['SENDMAIL_PASSWORD'],
+  #   # domain:         ENV['MAIL_HOST'],
+  #   domain:        "gmail.com",
+  #   address:       'smtp.gmail.com',
+  #   port:           587,
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true,
+  #   open_timeout: 5,
+  #   read_timeout: 5
+  # }
   config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    # domain:         ENV['MAIL_HOST'],
-    domain:        "gmail.com",
-    address:       'smtp.gmail.com',
-    port:           587,
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    open_timeout: 5,
-    read_timeout: 5
+      user_name: "apikey",
+      password: ENV['SENDGRID_API_KEY'],
+      domain: "gmail.com",
+      address: "smtp.sendgrid.net",
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
   }
 
   # Print deprecation notices to the Rails logger.
