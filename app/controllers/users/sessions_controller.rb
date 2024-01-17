539da8d9a45b4b955262f0a57cdaa323af30cf78
Bifabig <biftubig@gmail.com>
2024-01-17 17:35:21 +0300

    fix linters issues

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,7 +10,7 @@ class Users::SessionsController < Devise::SessionsController
       data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
     }, status: :ok
   end
-  
+
   def respond_to_on_destroy
     if current_user
       render json: {
