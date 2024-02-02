830a72ef04df4a5b2ec9a00ccc609bb304f32375
Bifabig <biftubig@gmail.com>
2024-02-02 15:51:55 +0300

    add authentication to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:index, :show, :create, :update, :new, :destroy]
-      resources :churches, only: [:index, :show, :create, :update, :new, :destroy]
-      resources :members, only: [:index, :show, :create, :update, :new, :destroy]
-      resources :teams, only: [:index, :show, :create, :update, :new, :destroy]
-      resources :programs, only: [:index, :show, :create, :update, :new, :destroy]
-      resources :attendances, only: [:index, :show, :create, :update, :new, :destroy]
-    end
-  end
-
+  # mount_devise_token_auth_for 'User', at: 'auth'
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -22,7 +12,21 @@ Rails.application.routes.draw do
     passwords: 'users/passwords'
   }
 
-  get '/current_user', to: 'current_user#index'
+  namespace :api do
+    namespace :v1 do
+      authenticated :user do
+        resources :current_user, only: [:index]
+        resources :users, only: [:index, :show, :create, :update, :new, :destroy]
+        resources :churches, only: [:index, :show, :create, :update, :new, :destroy]
+        resources :members, only: [:index, :show, :create, :update, :new, :destroy]
+        resources :teams, only: [:index, :show, :create, :update, :new, :destroy]
+        resources :programs, only: [:index, :show, :create, :update, :new, :destroy]
+        resources :attendances, only: [:index, :show, :create, :update, :new, :destroy]
+      end
+    end
+  end
+
+  # get '/current_user', to: 'current_user#index'
 
 
   # devise_for :users, controllers: {
